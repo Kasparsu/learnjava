2f76ed9f9930d826698aa26b54c1bc92b71ea69b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-10-11 20:42:45 +0300

    Add classes and excercise example

--- src/App.java
+++ src/App.java
@@ -1,103 +1,114 @@
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.TreeMap;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] nums = new int[10];
-        nums[0] = 2;
-        String[] names = {"kaspar", "martin", "suursalu"};
-        System.out.println(names);
-        String[] names2 = {"kaspar", "martin", "suursalu"};
-        System.out.println(names2[0]);
-        for (String name : names2) {
-            System.out.println(name);
+        // Animal.nr = 4;
+        // System.out.println(Animal.nr);
+        // Animal.getNumber();
+
+        Animal animal1 = new Animal("Miki Hiir", 2, 2);
+        // Animal.nr = 1;
+        //animal1.name = "Minni Hiir";
+        //animal1.age = 33;
+        // animal1.age = 56;
+        // animal1.nLegs = 2;
+        animal1.getDescription();
+        Animal animal2 = new Animal("Iiah", 5, 4);
+        // Animal.nr = 2;
+        // animal2.name = "Iiah";
+        // animal2.age = 5;
+        // animal2.nLegs = 4;
+        animal2.getDescription();
+        animal1.getDescription();
+        // System.out.println(Animal.nr);
+         System.out.println(animal2.getAge());
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+
+        School smartcod = new School("smartcod", 3);
+        Student ott = new Student("Ott Pikk", "some address", "ott@pikk.ee", "1234566778");
+        smartcod.addStudent(ott);
+        System.out.println(smartcod.getStudents());
+    }
+}
+
+class Animal {
+    public String name;
+    private Integer age;
+    public int nLegs;
+    public int id;
+    public static int nr = 0;
+
+
+
+    public Animal(String name, int age, int nLegs) {
+        this.name = name;
+        this.setAge(age);
+        this.nLegs = nLegs;
+        nr++;
+        this.id = nr;
+    }
+
+    public void getDescription() {
+        String hello = "hello";
+        System.out.printf("My name is %s and I am %d years old and I have %d legs and my number is %d\n", name, age, nLegs, id);
+    }
+    
+    public static void getNumber(){
+        
+        System.out.println(Animal.nr);
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        if(age>=0){
+            this.age = age;
+        } else {
+            throw new InvalidParameterException("Needs to have a positive number");
         }
+    }
 
-        ArrayList<String> fruits = new ArrayList<String>();
-        fruits.add("banana");
-        fruits.add("apple");
-        fruits.add("pineapple");
-        System.out.println(fruits.get(2));
-        int[][] array2d = new int[3][3];
-        array2d[0][0] = 3;
-        array2d[2][1] = 4;
-        System.out.println(array2d[0][0]);
-        String[][] board = {
-            {"1","2","3"},
-            {"4","5","6"},
-            {"7","8","9"}
-        };
-        board[1][1] = "x";
-        System.out.println(board[1][1]);
-        System.out.println(fruits.toArray()[2]);
-        PriorityQueue<String> queue = new PriorityQueue<String>(); // LIFO - Last in first out
-        queue.add("vello");
-        queue.add("malle");
-        queue.add("kati");
-        System.out.println(queue.poll()); // kui tühi tagastab null
-        System.out.println(queue.remove()); // kui tühi tagastab viskab exceptioni
-        System.out.println(queue.remove());
-        
+}
+class School {
+    private ArrayList<Student> students = new ArrayList<Student>();
+    private String name;
+    private int capacity;
 
-        LinkedList<String> queue2 = new LinkedList<String>(); // FIFO - first in first out
-        queue2.add("vello");
-        queue2.add("malle");
-        queue2.add("kati");
-        System.out.println(queue2.remove());
-        System.out.println(queue2.remove());
-        System.out.println(queue2.remove());
-        int example = 1;
-        //core 1
-        example++;//1+1
-        System.out.println(example);
-        //core 2
-        example++;//1+1
-        System.out.println(example);
-        
-        HashMap<String, String> map = new HashMap<String, String>();
+    public School(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
 
-        map.put("name", "Kaspar");
-        map.put("age", "12");
-        map.put("gender","male");
-        
-        System.out.println(map);
-        System.out.println(map.get("age"));
-        System.out.println(map.get("gender"));
+    public void addStudent(Student student) {
+        if(!this.students.contains(student) && this.students.size() < capacity){
+            this.students.add(student);
+        }
+    }
+    
+    public ArrayList<Student> getStudents() {
+        return this.students;
+    }
+    public int getCapacity() {
+        return this.capacity;
+    }
+    public String getName() {
+        return this.name;
+    }
+}
 
-        Map<String,Integer> map2 = Map.of("A", 1, "B", 2, "C", 3);
+class Student {
+    private String name;
+    private String address;
+    private String email;
+    private String idCode;
 
-        System.out.println(map2);
-        HashSet<String> cars = new HashSet<String>();
-        cars.add("volvo");
-        cars.add("volvo");
-        cars.add("saab");
-        cars.add("audi");
-        cars.add("volvo");
-        cars.add("saab");
-        cars.remove("audi");
-        System.out.println(cars);
-
-        TreeMap<Integer, String> scores = new TreeMap<Integer, String>();
-        scores.put(42, "Vello");
-        scores.put(12, "Annika");
-        scores.put(55, "Anna");
-        scores.put(62, "Felix");
-        System.out.println(scores);
-        scores.remove(55);
-        System.out.println(scores);
-        System.out.println(scores.descendingMap());
-        System.out.println(scores.headMap(42,true));
-        System.out.println(scores.tailMap(42,true));
-        scores.put(55, "Anna");
-        scores.put(54, "Anna");
-        System.out.println(scores.subMap(42,true,55,true));
+    public Student(String name, String address, String email, String idCode){
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.idCode = idCode;
     }
 }
