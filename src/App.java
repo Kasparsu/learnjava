d1f23fd1349e646e7e27d7e046114108d86389ae
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-10-13 20:12:03 +0300

    add Interface usage example

--- src/App.java
+++ src/App.java
@@ -1,87 +1,46 @@
-import java.util.Calendar;
-import java.time.LocalDate;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Student student = new Student("Kaspar", LocalDate.of(1993, 03, 21));
-        System.out.println(student.getBirthDate());
-        student.setMissedClasses(10);
-        System.out.println(student.getMissedClasses());
-
-        Person parent = new Person("Martin", LocalDate.of(1993, 03, 21));
-        student.sayHelloToStudent();
-
-        FirstYearStudent firstYearStudent = new FirstYearStudent("Manivald", LocalDate.of(1993, 03, 21));
-        System.out.println(firstYearStudent.getBirthDate());
-        firstYearStudent.setMissedClasses(10);
-        System.out.println(firstYearStudent.getMissedClasses());
-        firstYearStudent.sayHelloToStudent();
-        
-        Teacher teacher = new Teacher("Kaspar Martin", LocalDate.of(1993, 03, 21), 1100);
-        Teacher teacher2 = new Teacher("Kaspar Martin", LocalDate.of(1993, 03, 21), 1100);
-        Teacher teacher3 = new Teacher("Kaspar Martin", LocalDate.of(1993, 03, 21), 1100);
-        System.out.println(teacher.getName());
-        System.out.println(teacher.getSalary());
+        FileLogger logger = new FileLogger();
+        Task task = new Task(logger);
+        task.runTask();
     }
 }
 
-class Person {
-    protected String name;
-    private LocalDate birthdate;
+interface Logger {
+    public void log(String message);
+}
 
-    public Person(String name, LocalDate birthdate){
-        this.name = name;
-        this.birthdate = birthdate;
-    }
-
-    public String getName(){
-        return name;
-    }
-    
-    public LocalDate getBirthDate(){
-        return birthdate;
+class FileLogger implements Logger {
+    public void log(String message){
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt", true));
+            writer.append(message + "\n");
+            writer.close();
+        } catch (IOException e){
+            System.out.println(e);
+        }
     }
 }
 
-class Student extends Person {
-    private Integer missedClasses;
-
-    public Student(String name, LocalDate birthdate){
-        super(name, birthdate);
-    }
-
-    public void sayHelloToStudent(){
-        System.out.println("Hello " + name);
-    }
-
-    public Integer getMissedClasses(){
-        return missedClasses;
-    }
-
-    public void setMissedClasses(Integer missedClasses){
-        this.missedClasses = missedClasses;
+class TerminalLogger implements Logger {
+    public void log(String message){
+        System.out.println(message);
     }
 }
 
-class FirstYearStudent extends Student {
-    public FirstYearStudent(String name, LocalDate birthdate){
-        super(name, birthdate);
-    }
-}
+class Task {
+    Logger logger;
 
-class Teacher extends Person {
-    private Integer salary;
-
-    public Teacher(String name, LocalDate birthdate, Integer salary){
-        super(name, birthdate);
-        this.salary = salary;
+    public Task(Logger logger){
+        this.logger = logger;
     }
-    
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Integer salary) {
-        this.salary = salary;
+    public void runTask(){
+        for(Integer i=0;i<10;i++){
+            logger.log("Task nr " + i + " done successfully");
+        }
     }
 }
